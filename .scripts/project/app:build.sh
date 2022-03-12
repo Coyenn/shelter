20183f4a3fc8292ce9022830934313ce1872ae84
Tim Ritter <timrittermail@gmail.com>
2022-03-13 00:53:25 +0100

    Seperate production and dev workflow

---
+++ .scripts/project/app:build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# 👋 Build for production
+
+set -e
+cd "$(dirname "$0")"/../../ || exit
+
+./project app:down
+
+cd .docker || exit
+echo "Building docker images"
+docker-compose --env-file ../.env -f docker-compose.prod.yml build
+echo "Building Svelte Kit"
+docker-compose --env-file ../.env -f docker-compose.prod.yml run node yarn svelte-kit-build
+cd ../
