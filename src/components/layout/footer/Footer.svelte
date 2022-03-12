92b003c320eb5257cddd326df8c51bd43d19e0ab
Tim Ritter <timrittermail@gmail.com>
2022-03-12 17:08:16 +0100

    Boilerplate

---
+++ src/components/layout/footer/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+</style>
